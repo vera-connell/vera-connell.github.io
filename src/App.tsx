0a4a6e6b8f52ae228dc0fcd6e6858c8c696a257c
Finn Connell <finn.bc@protonmail.com>
2023-07-18 10:12:52 +1200

    configures hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to my portfolio!</h1>
+      <Outlet />
     </div>
   )
 }
---
+++ src/components/Thingy.tsx
@@ -0,0 +1,10 @@
+function Thingy() {
+  return (
+    <>
+      <h1>Thingamoaddo</h1>
+      <img src="images/2-resize.png" alt="my logo"></img>
+    </>
+  )
+}
+
+export default Thingy
