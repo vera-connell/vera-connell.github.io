c2807b29581ccef904b8eb8790dcc4feb962eca4
Finn Connell <finn.bc@protonmail.com>
2023-07-18 12:12:29 +1200

    sets up some styling and a cv download

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,11 +2,23 @@ import { Link } from 'react-router-dom'
 
 export function Home() {
   return (
-    <>
-      <Link to="/thing">
-        Thing <link rel="stylesheet" href="" />
-      </Link>
-      <h1>Home</h1>
-    </>
+    <div className="container">
+      <div className="grid-container">
+        <div className="item1">
+          <h1>Vera Connell</h1>
+          <p>web developer</p>
+        </div>
+        <div className="item2">Menu</div>
+        <div className="item3">
+          <img
+            className="app-logo"
+            src="images/magic-mykes-logo.png"
+            alt="magic-mykes-logo"
+          />
+        </div>
+        <div className="item4">Right</div>
+        <div className="item5">about</div>
+      </div>
+    </div>
   )
 }
